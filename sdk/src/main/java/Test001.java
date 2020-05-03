9bd86c28c327ee51aa1137e5b818d4242b64b41e
AidenDan <1299930560@qq.com>
2020-05-03 22:32:08 +0800

    1231

---
+++ sdk/src/main/java/Test001.java
@@ -0,0 +1,11 @@
+/**
+ * @author Aiden
+ * @version 1.0
+ * @description
+ * @date 2020-5-3 22:29:20
+ */
+public class Test001 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
