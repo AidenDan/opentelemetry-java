4811d2d6233806f33c2841156b282bcb4e278596
AidenDan <1299930560@qq.com>
2020-05-03 22:41:30 +0800

    323

--- sdk/src/main/java/Tets002.java
+++ sdk/src/main/java/Tets002.java
@@ -2,9 +2,9 @@
  * @author Aiden
  * @version 1.0
  * @description
- * @date 2020-5-3 22:29:20
+ * @date 2020-5-3 22:33:50
  */
-public class Test001 {
+public class Tets002 {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello");
